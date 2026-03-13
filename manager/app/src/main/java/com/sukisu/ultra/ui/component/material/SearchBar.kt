tiann/KernelSU

manager: Refactor manager screens to unified UiState and actions (#3283)
- Refactor Manager Compose screens to a unified coordinator pattern
based on `UiState + Actions`, shared by both Material and Miuix
implementations.
- Search behavior is now debounced and unified across multiple screens.
- App profile navigation now depends on UID-based group resolution
rather than explicit package name.
- Module update/uninstall confirmation and side effects are centralized
in the module coordinator.

---------

Signed-off-by: u9521 <63995396+u9521@users.noreply.github.com>
Co-authored-by: KOWX712 <leecc0503@gmail.com>
Co-authored-by: YuKongA <70465933+YuKongA@users.noreply.github.com>

manager/app/src/main/java/me/weishu/kernelsu/ui/component/material/SearchBar.kt
@@ -2,14 +2,20 @@ package me.weishu.kernelsu.ui.component.material
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.compose.material.icons.Icons
@@ -26,6 +32,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.SearchBarValue
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -40,12 +47,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
+import me.weishu.kernelsu.ui.util.LocalSnackbarHost
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -57,7 +67,7 @@ fun SearchAppBar(
     navigationIcon: @Composable (() -> Unit)? = null,
     actions: @Composable (() -> Unit)? = null,
     scrollBehavior: TopAppBarScrollBehavior? = null,
-    searchContent: @Composable ColumnScope.(closeSearch: () -> Unit) -> Unit = { }
+    searchContent: @Composable BoxScope.(bottomPadding: Dp, closeSearch: () -> Unit) -> Unit = { _, _ -> }
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
@@ -179,6 +189,7 @@ fun SearchAppBar(
             SearchBar(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal))
                     .padding(horizontal = 16.dp)
                     .padding(bottom = 8.dp),
                 state = searchBarState,
@@ -190,6 +201,7 @@ fun SearchAppBar(
     ExpandedFullScreenContainedSearchBar(
         state = searchBarState,
         inputField = inputField,
+        windowInsets = { SearchBarDefaults.fullScreenWindowInsets.only(WindowInsetsSides.Top + WindowInsetsSides.Horizontal) },
         colors = SearchBarDefaults.colors(
             containerColor = MaterialTheme.colorScheme.surfaceContainerHighest,
             inputFieldColors = SearchBarDefaults.inputFieldColors(
@@ -199,8 +211,20 @@ fun SearchAppBar(
             )
         ),
         content = {
-            if (currentQuery.isNotEmpty()) {
-                searchContent(collapseAndClear)
+            val snackBarHostState = LocalSnackbarHost.current
+            val bottomPadding = SearchBarDefaults.fullScreenWindowInsets.asPaddingValues().calculateBottomPadding()
+            Box(modifier = Modifier.fillMaxSize()) {
+                if (currentQuery.isNotEmpty()) {
+                    searchContent(bottomPadding, collapseAndClear)
+                }
+                SnackbarHost(
+                    hostState = snackBarHostState,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .navigationBarsPadding()
+                        .imePadding()
+                        .padding(bottom = 16.dp)
+                )
             }
         }
     )

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/executemoduleaction/ExecuteModuleActionUiState.kt
@@ -1,0 +1,14 @@
+package me.weishu.kernelsu.ui.screen.executemoduleaction
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class ExecuteModuleActionUiState(
+    val text: String,
+)
+
+@Immutable
+data class ExecuteModuleActionScreenActions(
+    val onBack: () -> Unit,
+    val onSaveLog: () -> Unit,
+)

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/superuser/SuperUserScreen.kt
@@ -1,18 +1,66 @@
 package me.weishu.kernelsu.ui.screen.superuser
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.Dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import me.weishu.kernelsu.ui.LocalUiMode
 import me.weishu.kernelsu.ui.UiMode
 import me.weishu.kernelsu.ui.navigation3.Navigator
+import me.weishu.kernelsu.ui.navigation3.Route
+import me.weishu.kernelsu.ui.viewmodel.SuperUserViewModel
 
 @Composable
 fun SuperUserPager(
     navigator: Navigator,
     bottomInnerPadding: Dp
 ) {
+    val viewModel = viewModel<SuperUserViewModel>()
+    val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(Unit) {
+        if (uiState.groupedApps.isEmpty()) {
+            viewModel.initializePreferences()
+            viewModel.loadAppList().join()
+        } else if (viewModel.isNeedRefresh) {
+            viewModel.loadAppList(resort = false).join()
+        }
+    }
+
+    val onSearchTextChange: (String) -> Unit = viewModel::updateSearchText
+    val onToggleShowSystemApps: () -> Unit = {
+        viewModel.toggleShowSystemApps()
+    }
+    val onToggleShowOnlyPrimaryUserApps: () -> Unit = {
+        viewModel.toggleShowOnlyPrimaryUserApps()
+    }
+    val onOpenProfile: (GroupedApps) -> Unit = { group ->
+        navigator.push(Route.AppProfile(group.uid))
+        viewModel.markNeedRefresh()
+    }
+    val actions = SuperUserActions(
+        onRefresh = { viewModel.loadAppList(force = true) },
+        onSearchTextChange = onSearchTextChange,
+        onSearchStatusChange = viewModel::updateSearchStatus,
+        onClearSearch = { onSearchTextChange("") },
+        onToggleShowSystemApps = onToggleShowSystemApps,
+        onToggleShowOnlyPrimaryUserApps = onToggleShowOnlyPrimaryUserApps,
+        onOpenProfile = onOpenProfile,
+    )
+
     when (LocalUiMode.current) {
-        UiMode.Miuix -> SuperUserPagerMiuix(navigator, bottomInnerPadding)
-        UiMode.Material -> SuperUserPagerMaterial(navigator, bottomInnerPadding)
+        UiMode.Miuix -> SuperUserPagerMiuix(
+            uiState = uiState,
+            actions = actions,
+            bottomInnerPadding = bottomInnerPadding,
+        )
+
+        UiMode.Material -> SuperUserPagerMaterial(
+            uiState = uiState,
+            actions = actions,
+            bottomInnerPadding = bottomInnerPadding,
+        )
     }
 }

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/MainActivityViewModel.kt
@@ -1,0 +1,61 @@
+package me.weishu.kernelsu.ui.viewmodel
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import me.weishu.kernelsu.data.repository.SettingsRepository
+import me.weishu.kernelsu.data.repository.SettingsRepositoryImpl
+import me.weishu.kernelsu.ksuApp
+import me.weishu.kernelsu.ui.UiMode
+import me.weishu.kernelsu.ui.theme.ThemeController
+
+class MainActivityViewModel : ViewModel() {
+
+    private val prefs = ksuApp.getSharedPreferences("settings", Context.MODE_PRIVATE)
+    private val settingRepo: SettingsRepository = SettingsRepositoryImpl()
+    private val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        if (key == null || key in observedKeys) {
+            _uiState.value = readUiState()
+        }
+    }
+
+    private val _uiState = MutableStateFlow(readUiState())
+    val uiState: StateFlow<MainActivityUiState> = _uiState.asStateFlow()
+
+    init {
+        prefs.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onCleared() {
+        prefs.unregisterOnSharedPreferenceChangeListener(listener)
+        super.onCleared()
+    }
+
+    private fun readUiState(): MainActivityUiState {
+        return MainActivityUiState(
+            appSettings = ThemeController.getAppSettings(ksuApp),
+            pageScale = settingRepo.pageScale,
+            enableBlur = settingRepo.enableBlur,
+            enableFloatingBottomBar = settingRepo.enableFloatingBottomBar,
+            enableFloatingBottomBarBlur = settingRepo.enableFloatingBottomBarBlur,
+            uiMode = UiMode.fromValue(settingRepo.uiMode),
+        )
+    }
+
+    private companion object {
+        val observedKeys = setOf(
+            "color_mode",
+            "key_color",
+            "color_style",
+            "color_spec",
+            "page_scale",
+            "enable_blur",
+            "enable_floating_bottom_bar",
+            "enable_floating_bottom_bar_blur",
+            "ui_mode",
+        )
+    }
+}

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/TemplateViewModel.kt
@@ -13,6 +13,7 @@ import me.weishu.kernelsu.data.repository.TemplateRepository
 import me.weishu.kernelsu.data.repository.TemplateRepositoryImpl
 import me.weishu.kernelsu.profile.Capabilities
 import me.weishu.kernelsu.profile.Groups
+import me.weishu.kernelsu.ui.screen.template.TemplateUiState
 import me.weishu.kernelsu.ui.util.getAppProfileTemplate
 import org.json.JSONArray
 import org.json.JSONObject

Testady/SukiSU-Ultra

Co-authored-by: ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>

manager/app/src/main/java/com/sukisu/ultra/ui/component/material/SearchBar.kt
@@ -2,14 +2,20 @@ package com.sukisu.ultra.ui.component.material
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.compose.material.icons.Icons
@@ -26,6 +32,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.SearchBarValue
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -40,12 +47,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
+import com.sukisu.ultra.ui.util.LocalSnackbarHost
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -57,7 +67,7 @@ fun SearchAppBar(
     navigationIcon: @Composable (() -> Unit)? = null,
     actions: @Composable (() -> Unit)? = null,
     scrollBehavior: TopAppBarScrollBehavior? = null,
-    searchContent: @Composable ColumnScope.(closeSearch: () -> Unit) -> Unit = { }
+    searchContent: @Composable BoxScope.(bottomPadding: Dp, closeSearch: () -> Unit) -> Unit = { _, _ -> }
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
@@ -179,6 +189,7 @@ fun SearchAppBar(
             SearchBar(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal))
                     .padding(horizontal = 16.dp)
                     .padding(bottom = 8.dp),
                 state = searchBarState,
@@ -190,6 +201,7 @@ fun SearchAppBar(
     ExpandedFullScreenContainedSearchBar(
         state = searchBarState,
         inputField = inputField,
+        windowInsets = { SearchBarDefaults.fullScreenWindowInsets.only(WindowInsetsSides.Top + WindowInsetsSides.Horizontal) },
         colors = SearchBarDefaults.colors(
             containerColor = MaterialTheme.colorScheme.surfaceContainerHighest,
             inputFieldColors = SearchBarDefaults.inputFieldColors(
@@ -199,8 +211,20 @@ fun SearchAppBar(
             )
         ),
         content = {
-            if (currentQuery.isNotEmpty()) {
-                searchContent(collapseAndClear)
+            val snackBarHostState = LocalSnackbarHost.current
+            val bottomPadding = SearchBarDefaults.fullScreenWindowInsets.asPaddingValues().calculateBottomPadding()
+            Box(modifier = Modifier.fillMaxSize()) {
+                if (currentQuery.isNotEmpty()) {
+                    searchContent(bottomPadding, collapseAndClear)
+                }
+                SnackbarHost(
+                    hostState = snackBarHostState,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .navigationBarsPadding()
+                        .imePadding()
+                        .padding(bottom = 16.dp)
+                )
             }
         }
     )

manager/app/src/main/java/com/sukisu/ultra/ui/screen/executemoduleaction/ExecuteModuleActionUiState.kt
@@ -1,0 +1,14 @@
+package com.sukisu.ultra.ui.screen.executemoduleaction
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class ExecuteModuleActionUiState(
+    val text: String,
+)
+
+@Immutable
+data class ExecuteModuleActionScreenActions(
+    val onBack: () -> Unit,
+    val onSaveLog: () -> Unit,
+)

manager/app/src/main/java/com/sukisu/ultra/ui/screen/superuser/SuperUserScreen.kt
@@ -1,18 +1,66 @@
 package com.sukisu.ultra.ui.screen.superuser
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.Dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sukisu.ultra.ui.LocalUiMode
 import com.sukisu.ultra.ui.UiMode
 import com.sukisu.ultra.ui.navigation3.Navigator
+import com.sukisu.ultra.ui.navigation3.Route
+import com.sukisu.ultra.ui.viewmodel.SuperUserViewModel
 
 @Composable
 fun SuperUserPager(
     navigator: Navigator,
     bottomInnerPadding: Dp
 ) {
+    val viewModel = viewModel<SuperUserViewModel>()
+    val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(Unit) {
+        if (uiState.groupedApps.isEmpty()) {
+            viewModel.initializePreferences()
+            viewModel.loadAppList().join()
+        } else if (viewModel.isNeedRefresh) {
+            viewModel.loadAppList(resort = false).join()
+        }
+    }
+
+    val onSearchTextChange: (String) -> Unit = viewModel::updateSearchText
+    val onToggleShowSystemApps: () -> Unit = {
+        viewModel.toggleShowSystemApps()
+    }
+    val onToggleShowOnlyPrimaryUserApps: () -> Unit = {
+        viewModel.toggleShowOnlyPrimaryUserApps()
+    }
+    val onOpenProfile: (GroupedApps) -> Unit = { group ->
+        navigator.push(Route.AppProfile(group.uid))
+        viewModel.markNeedRefresh()
+    }
+    val actions = SuperUserActions(
+        onRefresh = { viewModel.loadAppList(force = true) },
+        onSearchTextChange = onSearchTextChange,
+        onSearchStatusChange = viewModel::updateSearchStatus,
+        onClearSearch = { onSearchTextChange("") },
+        onToggleShowSystemApps = onToggleShowSystemApps,
+        onToggleShowOnlyPrimaryUserApps = onToggleShowOnlyPrimaryUserApps,
+        onOpenProfile = onOpenProfile,
+    )
+
     when (LocalUiMode.current) {
-        UiMode.Miuix -> SuperUserPagerMiuix(navigator, bottomInnerPadding)
-        UiMode.Material -> SuperUserPagerMaterial(navigator, bottomInnerPadding)
+        UiMode.Miuix -> SuperUserPagerMiuix(
+            uiState = uiState,
+            actions = actions,
+            bottomInnerPadding = bottomInnerPadding,
+        )
+
+        UiMode.Material -> SuperUserPagerMaterial(
+            uiState = uiState,
+            actions = actions,
+            bottomInnerPadding = bottomInnerPadding,
+        )
     }
 }

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/MainActivityViewModel.kt
@@ -1,0 +1,61 @@
+package com.sukisu.ultra.ui.viewmodel
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.sukisu.ultra.data.repository.SettingsRepository
+import com.sukisu.ultra.data.repository.SettingsRepositoryImpl
+import com.sukisu.ultra.ksuApp
+import com.sukisu.ultra.ui.UiMode
+import com.sukisu.ultra.ui.theme.ThemeController
+
+class MainActivityViewModel : ViewModel() {
+
+    private val prefs = ksuApp.getSharedPreferences("settings", Context.MODE_PRIVATE)
+    private val settingRepo: SettingsRepository = SettingsRepositoryImpl()
+    private val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        if (key == null || key in observedKeys) {
+            _uiState.value = readUiState()
+        }
+    }
+
+    private val _uiState = MutableStateFlow(readUiState())
+    val uiState: StateFlow<MainActivityUiState> = _uiState.asStateFlow()
+
+    init {
+        prefs.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onCleared() {
+        prefs.unregisterOnSharedPreferenceChangeListener(listener)
+        super.onCleared()
+    }
+
+    private fun readUiState(): MainActivityUiState {
+        return MainActivityUiState(
+            appSettings = ThemeController.getAppSettings(ksuApp),
+            pageScale = settingRepo.pageScale,
+            enableBlur = settingRepo.enableBlur,
+            enableFloatingBottomBar = settingRepo.enableFloatingBottomBar,
+            enableFloatingBottomBarBlur = settingRepo.enableFloatingBottomBarBlur,
+            uiMode = UiMode.fromValue(settingRepo.uiMode),
+        )
+    }
+
+    private companion object {
+        val observedKeys = setOf(
+            "color_mode",
+            "key_color",
+            "color_style",
+            "color_spec",
+            "page_scale",
+            "enable_blur",
+            "enable_floating_bottom_bar",
+            "enable_floating_bottom_bar_blur",
+            "ui_mode",
+        )
+    }
+}

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/TemplateViewModel.kt
@@ -13,6 +13,7 @@ import com.sukisu.ultra.data.repository.TemplateRepository
 import com.sukisu.ultra.data.repository.TemplateRepositoryImpl
 import com.sukisu.ultra.profile.Capabilities
 import com.sukisu.ultra.profile.Groups
+import com.sukisu.ultra.ui.screen.template.TemplateUiState
 import com.sukisu.ultra.ui.util.getAppProfileTemplate
 import org.json.JSONArray
 import org.json.JSONObject