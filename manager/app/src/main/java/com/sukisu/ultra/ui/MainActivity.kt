tiann/KernelSU

manager: bump miuix to 0.8.8

manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
@@ -31,7 +31,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -115,7 +115,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val viewModel = viewModel<MainActivityViewModel>()
-            val uiState by viewModel.uiState.collectAsState()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
             val appSettings = uiState.appSettings
             val uiMode = uiState.uiMode
             val darkMode = appSettings.colorMode.isDark || (appSettings.colorMode.isSystem && isSystemInDarkTheme())
@@ -153,7 +153,7 @@ class MainActivity : ComponentActivity() {
                 LocalSnackbarHost provides snackBarHostState
             ) {
                 KernelSUTheme(appSettings = appSettings, uiMode = uiMode) {
-                    HandleDeepLink(intentState = intentState.collectAsState())
+                    HandleDeepLink(intentState = intentState.collectAsStateWithLifecycle())
                     ZipFileIntentHandler(intentState = intentState, isManager = isManager)
                     ShortcutIntentHandler(intentState = intentState)
 
@@ -260,20 +260,22 @@ fun MainScreen() {
     CompositionLocalProvider(
         LocalMainPagerState provides mainPagerState
     ) {
+        val contentReady = me.weishu.kernelsu.ui.util.rememberContentReady()
         val pagerContent = @Composable { bottomInnerPadding: Dp ->
             HorizontalPager(
                 modifier = Modifier
                     .then(if (enableBlur) Modifier.hazeSource(state = hazeState) else Modifier)
                     .then(if (enableFloatingBottomBar && enableFloatingBottomBarBlur) Modifier.layerBackdrop(backdrop) else Modifier),
                 state = mainPagerState.pagerState,
-                beyondViewportPageCount = 3,
+                beyondViewportPageCount = if (contentReady) 3 else 0,
                 userScrollEnabled = userScrollEnabled,
-            ) {
-                when (it) {
-                    0 -> HomePager(navController, bottomInnerPadding)
-                    1 -> SuperUserPager(navController, bottomInnerPadding)
-                    2 -> ModulePager(bottomInnerPadding)
-                    3 -> SettingPager(navController, bottomInnerPadding)
+            ) { page ->
+                val isCurrentPage = page == mainPagerState.pagerState.currentPage
+                when (page) {
+                    0 -> if (isCurrentPage || contentReady) HomePager(navController, bottomInnerPadding)
+                    1 -> if (isCurrentPage || contentReady) SuperUserPager(navController, bottomInnerPadding)
+                    2 -> if (isCurrentPage || contentReady) ModulePager(bottomInnerPadding)
+                    3 -> if (isCurrentPage || contentReady) SettingPager(navController, bottomInnerPadding)
                 }
             }
         }
@@ -396,7 +398,7 @@ private fun ZipFileIntentHandler(
         return uri.getFileName(context) ?: uri.lastPathSegment ?: "Unknown"
     }
 
-    val intentStateValue by intentState.collectAsState()
+    val intentStateValue by intentState.collectAsStateWithLifecycle()
     LaunchedEffect(intentStateValue) {
         val currentIntent = activity.intent
         val uri = currentIntent?.data ?: return@LaunchedEffect
@@ -429,7 +431,7 @@ private fun ShortcutIntentHandler(
 ) {
     val activity = LocalActivity.current ?: return
     val context = LocalContext.current
-    val intentStateValue by intentState.collectAsState()
+    val intentStateValue by intentState.collectAsStateWithLifecycle()
     val navigator = LocalNavigator.current
     LaunchedEffect(intentStateValue) {
         val intent = activity.intent

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMiuix.kt
@@ -55,6 +55,7 @@ import me.weishu.kernelsu.ui.component.rebootlistpopup.RebootListPopupMiuix
 import me.weishu.kernelsu.ui.theme.LocalEnableBlur
 import me.weishu.kernelsu.ui.theme.isInDarkTheme
 import me.weishu.kernelsu.ui.util.defaultHazeEffect
+import me.weishu.kernelsu.ui.util.module.LatestVersionInfo
 import top.yukonga.miuix.kmp.basic.BasicComponent
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
 import top.yukonga.miuix.kmp.basic.Card
@@ -128,9 +129,9 @@ fun HomePagerMiuix(
                     }
                     if (state.showVersionMismatchWarning) {
                         WarningCard(
-                            stringResource(id = R.string.home_version_mismatch).format(
+                            stringResource(id = R.string.home_version_mismatch,
                                 state.currentManagerVersionCode,
-                                state.ksuVersion
+                                state.ksuVersion ?: 0
                             )
                         )
                     }
@@ -139,8 +140,8 @@ fun HomePagerMiuix(
                     }
                     if (state.showRequireKernelWarning) {
                         WarningCard(
-                            stringResource(id = R.string.require_kernel_version)
-                                .format(state.ksuVersion, me.weishu.kernelsu.Natives.MINIMAL_SUPPORTED_KERNEL),
+                            stringResource(id = R.string.require_kernel_version,
+                                state.ksuVersion ?: 0, me.weishu.kernelsu.Natives.MINIMAL_SUPPORTED_KERNEL),
                         )
                     }
                     if (state.showRootWarning) {
@@ -150,7 +151,6 @@ fun HomePagerMiuix(
                         state = state,
                         actions = actions,
                     )
-
                     if (state.checkUpdateEnabled) {
                         UpdateCard(state = state, actions = actions)
                     }
@@ -180,7 +180,7 @@ private fun UpdateCard(
     ) {
         val updateDialog = rememberConfirmDialog(onConfirm = { actions.onOpenUrl(newVersion.downloadUrl) })
         WarningCard(
-            message = stringResource(id = R.string.new_version_available).format(newVersion.versionCode),
+            message = stringResource(id = R.string.new_version_available, newVersion.versionCode),
             colorScheme.outline
         ) {
             if (newVersion.changelog.isEmpty()) {
@@ -396,7 +396,6 @@ private fun StatusCard(
                                 if (state.isSELinuxPermissive) {
                                     TextButton(
                                         text = stringResource(R.string.home_jailbreak),
-                                        insideMargin = PaddingValues(12.dp),
                                         onClick = actions.onJailbreakClick,
                                         colors = ButtonDefaults.textButtonColorsPrimary()
                                     )
@@ -538,9 +537,15 @@ private fun InfoCard(systemInfo: SystemInfo) {
             InfoText(title = stringResource(R.string.home_kernel), content = systemInfo.kernelVersion)
             InfoText(title = stringResource(R.string.home_manager_version), content = systemInfo.managerVersion)
             InfoText(title = stringResource(R.string.home_fingerprint), content = systemInfo.fingerprint)
+            val selinuxDisplay = when (systemInfo.selinuxStatus) {
+                "Enforcing" -> stringResource(R.string.selinux_status_enforcing)
+                "Permissive" -> stringResource(R.string.selinux_status_permissive)
+                "Disabled" -> stringResource(R.string.selinux_status_disabled)
+                else -> stringResource(R.string.selinux_status_unknown)
+            }
             InfoText(
                 title = stringResource(R.string.home_selinux_status),
-                content = systemInfo.selinuxStatus,
+                content = selinuxDisplay,
                 bottomPadding = 0.dp
             )
         }
@@ -566,7 +571,7 @@ private fun StatusCardNotActivatedPreview() {
 @Composable
 private fun StatusCardPermissivePreview() {
     StatusCard(
-        state = previewHomeScreenState(ksuVersion = null, lkmMode = null, isSELinuxPermissive = true),
+        state = previewHomeScreenState(ksuVersion = null, lkmMode = null, selinuxStatus = "Permissive"),
         actions = HomeActions({}, {}, {}, {})
     )
 }
@@ -597,7 +602,6 @@ private fun HomeScreenPreviewContent(
     lkmMode: Boolean?,
     isSafeMode: Boolean = false,
     isLateLoadMode: Boolean = false,
-    isSELinuxPermissive: Boolean = false,
     superuserCount: Int = 0,
     moduleCount: Int = 0,
     selinuxStatus: String = "Enforcing",
@@ -615,7 +619,6 @@ private fun HomeScreenPreviewContent(
                     lkmMode = lkmMode,
                     isSafeMode = isSafeMode,
                     isLateLoadMode = isLateLoadMode,
-                    isSELinuxPermissive = isSELinuxPermissive,
                     superuserCount = superuserCount,
                     moduleCount = moduleCount,
                     selinuxStatus = selinuxStatus,
@@ -644,7 +647,7 @@ private fun HomeScreenNotActivatedPreview() {
 @Preview(name = "Home Permissive", showBackground = true)
 @Composable
 private fun HomeScreenPermissivePreview() {
-    HomeScreenPreviewContent(ksuVersion = null, lkmMode = null, isSELinuxPermissive = true, selinuxStatus = "Permissive")
+    HomeScreenPreviewContent(ksuVersion = null, lkmMode = null, selinuxStatus = "Permissive")
 }
 
 @Preview(name = "Home Jailbreak", showBackground = true)
@@ -658,7 +661,6 @@ private fun previewHomeScreenState(
     lkmMode: Boolean?,
     isSafeMode: Boolean = false,
     isLateLoadMode: Boolean = false,
-    isSELinuxPermissive: Boolean = false,
     superuserCount: Int = 0,
     moduleCount: Int = 0,
     selinuxStatus: String = "Enforcing",
@@ -673,9 +675,8 @@ private fun previewHomeScreenState(
     isRootAvailable = ksuVersion != null,
     isSafeMode = isSafeMode,
     isLateLoadMode = isLateLoadMode,
-    isSELinuxPermissive = isSELinuxPermissive,
     checkUpdateEnabled = false,
-    latestVersionInfo = me.weishu.kernelsu.ui.util.module.LatestVersionInfo(),
+    latestVersionInfo = LatestVersionInfo(),
     currentManagerVersionCode = 10000,
     superuserCount = superuserCount,
     moduleCount = moduleCount,

Testady/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/MainActivity.kt
@@ -30,7 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -128,7 +128,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val viewModel = viewModel<MainActivityViewModel>()
-            val uiState by viewModel.uiState.collectAsState()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
             val appSettings = uiState.appSettings
             val uiMode = uiState.uiMode
             val darkMode = appSettings.colorMode.isDark || (appSettings.colorMode.isSystem && isSystemInDarkTheme())
@@ -166,7 +166,7 @@ class MainActivity : ComponentActivity() {
                 LocalSnackbarHost provides snackBarHostState
             ) {
                 KernelSUTheme(appSettings = appSettings, uiMode = uiMode) {
-                    HandleDeepLink(intentState = intentState.collectAsState())
+                    HandleDeepLink(intentState = intentState.collectAsStateWithLifecycle())
                     ShortcutIntentHandler(intentState = intentState)
                     HandleZipFileIntent(intentState = intentState)
 
@@ -285,20 +285,22 @@ fun MainScreen() {
     CompositionLocalProvider(
         LocalMainPagerState provides mainPagerState
     ) {
+        val contentReady = com.sukisu.ultra.ui.util.rememberContentReady()
         val pagerContent = @Composable { bottomInnerPadding: Dp ->
             HorizontalPager(
                 modifier = Modifier
                     .then(if (enableBlur) Modifier.hazeSource(state = hazeState) else Modifier)
                     .then(if (enableFloatingBottomBar && enableFloatingBottomBarBlur) Modifier.layerBackdrop(backdrop) else Modifier),
                 state = mainPagerState.pagerState,
-                beyondViewportPageCount = 3,
+                beyondViewportPageCount = if (contentReady) 3 else 0,
                 userScrollEnabled = userScrollEnabled,
-            ) {
-                when (it) {
-                    0 -> HomePager(navController, bottomInnerPadding)
-                    1 -> SuperUserPager(navController, bottomInnerPadding)
-                    2 -> ModulePager(bottomInnerPadding)
-                    3 -> SettingPager(navController, bottomInnerPadding)
+            ) { page ->
+                val isCurrentPage = page == mainPagerState.pagerState.currentPage
+                when (page) {
+                    0 -> if (isCurrentPage || contentReady) HomePager(navController, bottomInnerPadding)
+                    1 -> if (isCurrentPage || contentReady) SuperUserPager(navController, bottomInnerPadding)
+                    2 -> if (isCurrentPage || contentReady) ModulePager(bottomInnerPadding)
+                    3 -> if (isCurrentPage || contentReady) SettingPager(navController, bottomInnerPadding)
                 }
             }
         }
@@ -396,7 +398,7 @@ private fun ShortcutIntentHandler(
 ) {
     val activity = LocalActivity.current ?: return
     val context = LocalContext.current
-    val intentStateValue by intentState.collectAsState()
+    val intentStateValue by intentState.collectAsStateWithLifecycle()
     val navigator = LocalNavigator.current
     LaunchedEffect(intentStateValue) {
         val intent = activity.intent

manager/app/src/main/java/com/sukisu/ultra/ui/screen/home/HomeMiuix.kt
@@ -55,6 +55,7 @@ import com.sukisu.ultra.ui.component.rebootlistpopup.RebootListPopupMiuix
 import com.sukisu.ultra.ui.theme.LocalEnableBlur
 import com.sukisu.ultra.ui.theme.isInDarkTheme
 import com.sukisu.ultra.ui.util.defaultHazeEffect
+import com.sukisu.ultra.ui.util.module.LatestVersionInfo
 import top.yukonga.miuix.kmp.basic.BasicComponent
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
 import top.yukonga.miuix.kmp.basic.Card
@@ -128,16 +129,16 @@ fun HomePagerMiuix(
                     }
                     if (state.showVersionMismatchWarning) {
                         WarningCard(
-                            stringResource(id = R.string.home_version_mismatch).format(
+                            stringResource(id = R.string.home_version_mismatch,
                                 state.currentManagerVersionCode,
-                                state.ksuVersion
+                                state.ksuVersion ?: 0
                             )
                         )
                     }
                     if (state.showRequireKernelWarning) {
                         WarningCard(
-                            stringResource(id = R.string.require_kernel_version)
-                                .format(state.ksuVersion, com.sukisu.ultra.Natives.MINIMAL_SUPPORTED_KERNEL),
+                            stringResource(id = R.string.require_kernel_version,
+                                state.ksuVersion ?: 0, com.sukisu.ultra.Natives.MINIMAL_SUPPORTED_KERNEL),
                         )
                     }
                     if (state.showRootWarning) {
@@ -147,7 +148,6 @@ fun HomePagerMiuix(
                         state = state,
                         actions = actions,
                     )
-
                     if (state.checkUpdateEnabled) {
                         UpdateCard(state = state, actions = actions)
                     }
@@ -177,7 +177,7 @@ private fun UpdateCard(
     ) {
         val updateDialog = rememberConfirmDialog(onConfirm = { actions.onOpenUrl(newVersion.downloadUrl) })
         WarningCard(
-            message = stringResource(id = R.string.new_version_available).format(newVersion.versionCode),
+            message = stringResource(id = R.string.new_version_available, newVersion.versionCode),
             colorScheme.outline
         ) {
             if (newVersion.changelog.isEmpty()) {
@@ -393,7 +393,6 @@ private fun StatusCard(
                                 if (state.isSELinuxPermissive) {
                                     TextButton(
                                         text = stringResource(R.string.home_jailbreak),
-                                        insideMargin = PaddingValues(12.dp),
                                         onClick = actions.onJailbreakClick,
                                         colors = ButtonDefaults.textButtonColorsPrimary()
                                     )
@@ -546,9 +545,15 @@ private fun InfoCard(systemInfo: SystemInfo) {
             } else {
                 InfoText(title = stringResource(R.string.hook_type),content = hookTypeLabel)
             }
+            val selinuxDisplay = when (systemInfo.selinuxStatus) {
+                "Enforcing" -> stringResource(R.string.selinux_status_enforcing)
+                "Permissive" -> stringResource(R.string.selinux_status_permissive)
+                "Disabled" -> stringResource(R.string.selinux_status_disabled)
+                else -> stringResource(R.string.selinux_status_unknown)
+            }
             InfoText(
                 title = stringResource(R.string.home_selinux_status),
-                content = systemInfo.selinuxStatus,
+                content = selinuxDisplay,
             )
             InfoText(
                 title = stringResource(R.string.home_fingerprint),
@@ -578,7 +583,7 @@ private fun StatusCardNotActivatedPreview() {
 @Composable
 private fun StatusCardPermissivePreview() {
     StatusCard(
-        state = previewHomeScreenState(ksuVersion = null, lkmMode = null, isSELinuxPermissive = true),
+        state = previewHomeScreenState(ksuVersion = null, lkmMode = null, selinuxStatus = "Permissive"),
         actions = HomeActions({}, {}, {}, {})
     )
 }
@@ -609,7 +614,6 @@ private fun HomeScreenPreviewContent(
     lkmMode: Boolean?,
     isSafeMode: Boolean = false,
     isLateLoadMode: Boolean = false,
-    isSELinuxPermissive: Boolean = false,
     superuserCount: Int = 0,
     moduleCount: Int = 0,
     selinuxStatus: String = "Enforcing",
@@ -627,7 +631,6 @@ private fun HomeScreenPreviewContent(
                     lkmMode = lkmMode,
                     isSafeMode = isSafeMode,
                     isLateLoadMode = isLateLoadMode,
-                    isSELinuxPermissive = isSELinuxPermissive,
                     superuserCount = superuserCount,
                     moduleCount = moduleCount,
                     selinuxStatus = selinuxStatus,
@@ -656,7 +659,7 @@ private fun HomeScreenNotActivatedPreview() {
 @Preview(name = "Home Permissive", showBackground = true)
 @Composable
 private fun HomeScreenPermissivePreview() {
-    HomeScreenPreviewContent(ksuVersion = null, lkmMode = null, isSELinuxPermissive = true, selinuxStatus = "Permissive")
+    HomeScreenPreviewContent(ksuVersion = null, lkmMode = null, selinuxStatus = "Permissive")
 }
 
 @Preview(name = "Home Jailbreak", showBackground = true)
@@ -670,7 +673,6 @@ private fun previewHomeScreenState(
     lkmMode: Boolean?,
     isSafeMode: Boolean = false,
     isLateLoadMode: Boolean = false,
-    isSELinuxPermissive: Boolean = false,
     superuserCount: Int = 0,
     moduleCount: Int = 0,
     selinuxStatus: String = "Enforcing",
@@ -685,9 +687,8 @@ private fun previewHomeScreenState(
     isRootAvailable = ksuVersion != null,
     isSafeMode = isSafeMode,
     isLateLoadMode = isLateLoadMode,
-    isSELinuxPermissive = isSELinuxPermissive,
     checkUpdateEnabled = false,
-    latestVersionInfo = com.sukisu.ultra.ui.util.module.LatestVersionInfo(),
+    latestVersionInfo = LatestVersionInfo(),
     currentManagerVersionCode = 10000,
     superuserCount = superuserCount,
     moduleCount = moduleCount,